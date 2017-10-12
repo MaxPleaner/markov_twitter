777834edf111e02152c03fd01a639f8ba9751b71
maxpleaner <maxpleaner@gmail.com>
2017-10-12 13:59:55 -0700

    test the naive markov evaluator using seeded randomness

--- spec/markov_twitter/markov_builder_spec.rb
+++ spec/markov_twitter/markov_builder_spec.rb
@@ -3,15 +3,19 @@ MarkovBuilder = MarkovTwitter::MarkovBuilder
 
 RSpec.describe MarkovBuilder do
 
-  let(:phrases) { get_sample_user_first_9_tweets }
-
   it "processes phrases and stores their values/linkages" do
+    srand(0) # seeds the randomness
     phrase1 = "the cat in the hat"
     phrase2 = "the bat in the flat"
     markov_builder = MarkovBuilder.new(phrases: [phrase1, phrase2])
     expect(markov_builder.nodes.keys.sort).to eq(%w{bat cat flat hat in the})
-    y = 10.times.map { markov_builder.evaluate(length: 5) }
-    binding.pry
+    results = 3.times.map { markov_builder.evaluate(length: 5) }
+    expect(results).to eq [
+      "bat in the hat the",
+      "hat hat cat in the",
+      "in the flat the cat"
+    ]
+    srand() # unseeds the randomness
   end
 
 end
